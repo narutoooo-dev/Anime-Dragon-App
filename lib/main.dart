9deba3ab10433bc819e87c58da70c39082a65bde
Narutoooo_Dev <flashstore801@gmail.com>
2026-08-29 17:44:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:get_secure_storage/get_secure_storage.dart';
-
 import 'app_startup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'providers/local_auth_data_source.dart';
 import 'utils/provider_logger.dart';
+import 'services/supabase_service.dart'; // <-- أضف هذا السطر الجديد
 
-
-initBox() async{
+initBox() async {
     await GetSecureStorage.init(password: EnvironmentConfig.boxPassword);
 }
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // تهيئة Supabase (إضافة جديدة)
+  await SupabaseService().init();
+  
+  // باقي التهيئة القديمة
   await initBox();
-  runApp(ProviderScope(observers: [ProviderLogger()], child: const AppStartupWidget()));
+  
+  runApp(ProviderScope(
+    observers: [ProviderLogger()], 
+    child: const AppStartupWidget()
+  ));
 }
